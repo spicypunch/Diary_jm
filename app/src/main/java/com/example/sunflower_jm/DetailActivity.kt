e9c20c18cf5ef30cd26dc088590d33d02aa57acd
User <miniguni66@naver.com>
2022-11-18 14:04:17 +0900

    commit

--- app/src/main/java/com/example/sunflower_jm/DetailActivity.kt
+++ app/src/main/java/com/example/sunflower_jm/DetailActivity.kt
@@ -2,20 +2,27 @@ package com.example.sunflower_jm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sunflower_jm.databinding.DetailViewBinding
+import com.example.sunflower_jm.databinding.ItemBinding
 import com.example.sunflower_jm.db.SunFlowerEntity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.detail_view.*
 
 class DetailActivity : AppCompatActivity(){
     private lateinit var datas : SunFlowerEntity
+    private lateinit var binding: DetailViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.detail_view)
+        binding = DetailViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         datas = intent.getSerializableExtra("data") as SunFlowerEntity
 
-        detail_title.text = datas.title.toString()
-        detail_content.text = datas.content.toString()
+        detail_title.text = datas.title
+        detail_content.text = datas.content
+
+//        intent.getStringExtra("title")
+//        intent.getStringExtra("content")
     }
 
 }
--- app/src/main/java/com/example/sunflower_jm/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/sunflower_jm/RecyclerViewAdapter.kt
@@ -2,44 +2,47 @@ package com.example.sunflower_jm
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sunflower_jm.databinding.ItemBinding
 import com.example.sunflower_jm.db.SunFlowerEntity
 
-class RecyclerViewAdapter(private val itemList: ArrayList<SunFlowerEntity>) : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>(){
+class RecyclerViewAdapter(private val itemList: ArrayList<SunFlowerEntity>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
-                val item_title = binding.itemTitle
-                val item_content = binding.itemContent
-                val root = binding.root
+    class MyViewHolder(binding: ItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val item_title = binding.itemTitle
+        val item_content = binding.itemContent
+        val root = binding.root
 
         //detailview 페이지로 이동
-//        fun bind(item: SunFlowerEntity) {
-//            item_title.text = item.title.toString()
-//            item_content.text = item.content.toString()
-//            itemView.setOnClickListener {
-//                Intent(context, DetailActivity::class.java).apply {
-//                    putExtra("data", item.id)
-//                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//                }.run { context.startActivity(this) }
-//            }
-//        }
+        fun bind(item: SunFlowerEntity) {
+//            Log.e("잘 나오고 있어요~~~~~~~~~~~", "잘 나오고 있어요@")
+            item_title.text = item.title
+            item_content.text = item.content
+
+            itemView.setOnClickListener {
+                Intent(root.context, DetailActivity::class.java).apply {
+                    putExtra("data", item.id)
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }.run { root.context.startActivity(this) }
             }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val binding: ItemBinding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding: ItemBinding =
+            ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val sunflowerData = itemList[position]
-        holder.item_title.text = sunflowerData.title.toString()
-        holder.item_content.text = sunflowerData.content.toString()
+        holder.item_title.text = sunflowerData.title
+        holder.item_content.text = sunflowerData.content
     }
 
-    override fun getItemCount(): Int {
-        return itemList.size
-    }
+    override fun getItemCount(): Int = itemList.size
 }

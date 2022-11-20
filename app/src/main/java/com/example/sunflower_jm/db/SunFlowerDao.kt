ebfd278f9fc7a03f9f0a1837732a9145d45108a4
minig <miniguni66@naver.com>
2022-11-20 18:52:24 +0900

    add delete, update function

--- app/src/main/java/com/example/sunflower_jm/db/SunFlowerDao.kt
+++ app/src/main/java/com/example/sunflower_jm/db/SunFlowerDao.kt
@@ -11,6 +11,9 @@ interface SunFlowerDao {
     @Query("SELECT * FROM SunFlowerEntity")
     fun getAll(): List<SunFlowerEntity>
 
+//    @Query("SELECT id FROM SunFlowerEntity WHERE title = :gettitle AND :getcontent")
+//    fun getID(gettitle: String, getcontent: String) : Int
+
     @Insert
     fun insertItem(item: SunFlowerEntity)
 
@@ -20,5 +23,5 @@ interface SunFlowerDao {
     @Update
     fun updateItem(item: SunFlowerEntity)
 
-//    @Query("DELETE FROM SunFlowerEntity WHERE id = ")
+//    @Query("DELETE FROM SunFlowerEntity WHERE id = :mld")
 }
